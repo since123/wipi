290c0b2012459d98ae3fd67fa1a90e608e6d90c1
zhxuc <zhxuc7@163.com>
2020-02-06 14:04:09 +0800

    feat(client): 后台新增页面管理

---
+++ client/pages/admin/page/editor/[id].tsx
@@ -0,0 +1,162 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { NextPage } from "next";
+import { Button, Input, message } from "antd";
+import { AdminLayout } from "@/layout/AdminLayout";
+import { FileSelectDrawer } from "@/components/admin/FileSelectDrawer";
+import { PageProvider } from "@providers/page";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import style from "./index.module.scss";
+
+interface IProps {
+  page: IPage;
+}
+
+const Editor: NextPage<IProps> = ({ page: defaultPage = {} }) => {
+  const [mounted, setMounted] = useState(false);
+  const [fileDrawerVisible, setFileDrawerVisible] = useState(false);
+  const [id, setId] = useState(defaultPage.id);
+  const [page, setPage] = useState<any>(defaultPage);
+
+  useEffect(() => {
+    setMounted(true);
+
+    return () => {
+      setMounted(false);
+    };
+  }, []);
+
+  const save = useCallback(() => {
+    if (!page.name) {
+      message.warn("请输入页面名称");
+      return;
+    }
+
+    page.status = "draft";
+
+    if (id) {
+      return PageProvider.updatePage(id, page).then(res => {
+        setId(res.id);
+        message.success("页面已保存");
+      });
+    } else {
+      return PageProvider.addPage(page).then(res => {
+        setId(res.id);
+        message.success("页面已保存");
+      });
+    }
+  }, [page, id]);
+
+  const preview = useCallback(() => {
+    if (id) {
+      window.open("/page/" + id);
+    } else {
+      message.warn("请先保存");
+    }
+  }, [id]);
+
+  const publish = useCallback(() => {
+    let canPublish = true;
+    void [
+      ["name", "请输入页面名称"],
+      ["path", "请输入页面路径"],
+      ["content", "请输入页面内容"]
+    ].forEach(([key, msg]) => {
+      if (!page[key]) {
+        message.warn(msg);
+        canPublish = false;
+      }
+    });
+
+    if (!canPublish) {
+      return;
+    }
+
+    page.status = "publish";
+
+    if (id) {
+      return PageProvider.updatePage(id, page).then(res => {
+        setId(res.id);
+        message.success("页面已保存");
+      });
+    } else {
+      return PageProvider.addPage(page).then(res => {
+        setId(res.id);
+        message.success("页面已保存");
+      });
+    }
+  }, [page, id]);
+
+  return (
+    <AdminLayout>
+      <div className={style.wrapper}>
+        <Input
+          placeholder="请输入页面名称"
+          defaultValue={page.name}
+          onChange={e => {
+            setPage(page => {
+              const value = e.target.value;
+              page.name = value;
+              return page;
+            });
+          }}
+        />
+        <Input
+          style={{ marginTop: 16 }}
+          placeholder="请输入页面路径"
+          defaultValue={page.path}
+          onChange={e => {
+            setPage(page => {
+              const value = e.target.value;
+              page.path = value;
+              return page;
+            });
+          }}
+        />
+        {mounted && (
+          <SimpleMDE
+            className={style.formItem}
+            value={page.content}
+            onChange={value => {
+              setPage(page => {
+                page.content = value;
+                return page;
+              });
+            }}
+          />
+        )}
+        <FileSelectDrawer
+          isCopy={true}
+          closeAfterClick={true}
+          visible={fileDrawerVisible}
+          onClose={() => {
+            setFileDrawerVisible(false);
+          }}
+        />
+        <div className={style.operation}>
+          <Button
+            type="dashed"
+            onClick={() => {
+              setFileDrawerVisible(true);
+            }}
+          >
+            文件库
+          </Button>
+          <Button onClick={save}>保存</Button>
+          <Button onClick={preview}>预览</Button>
+          <Button type="primary" onClick={publish}>
+            发布
+          </Button>
+        </div>
+      </div>
+    </AdminLayout>
+  );
+};
+
+Editor.getInitialProps = async ctx => {
+  const { id } = ctx.query;
+  const page = await PageProvider.getPage(id);
+  return { page };
+};
+
+export default Editor;
---
+++ client/pages/admin/page/index.tsx
@@ -0,0 +1,136 @@
+import React, { useState, useCallback } from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Table, Button, Tag, Divider, Badge, Popconfirm, message } from "antd";
+import * as dayjs from "dayjs";
+import { AdminLayout } from "@/layout/AdminLayout";
+import { PageProvider } from "@providers/page";
+import style from "./index.module.scss";
+
+const columns = [
+  {
+    title: "名称",
+    dataIndex: "name",
+    key: "name",
+    render: (text, record) => (
+      <Link href={`/page/${record.path}`}>
+        <a>{text}</a>
+      </Link>
+    )
+  },
+  {
+    title: "路径",
+    dataIndex: "path",
+    key: "path"
+  },
+  {
+    title: "状态",
+    dataIndex: "status",
+    key: "status",
+    render: status => {
+      const isDraft = status === "draft";
+      return (
+        <Badge
+          color={isDraft ? "gold" : "green"}
+          text={isDraft ? "草稿" : "已发布"}
+        />
+      );
+    }
+  },
+  {
+    title: "发布时间",
+    dataIndex: "publishAt",
+    key: "publishAt",
+    render: date => dayjs.default(date).format("YYYY-MM-DD HH:mm:ss")
+  }
+];
+
+interface IProps {
+  pages: IPage[];
+}
+
+const Page: NextPage<IProps> = ({ pages: defaultPages = [] }) => {
+  const router = useRouter();
+  const [pages, setPages] = useState<IPage[]>(defaultPages);
+
+  const getPages = useCallback(() => {
+    PageProvider.getPages().then(pages => {
+      setPages(pages);
+    });
+  }, []);
+
+  const deleteArticle = useCallback(id => {
+    PageProvider.deletePage(id).then(() => {
+      message.success("页面删除成功");
+      getPages();
+    });
+  }, []);
+
+  const editPage = useCallback((id, data) => {
+    PageProvider.updatePage(id, data).then(() => {
+      message.success("操作成功");
+      getPages();
+    });
+  }, []);
+
+  const actionColumn = {
+    title: "操作",
+    key: "action",
+    render: (_, record) => {
+      const isDraft = record.status === "draft";
+
+      return (
+        <span>
+          {isDraft ? (
+            <a onClick={() => editPage(record.id, { status: "publish" })}>
+              启用
+            </a>
+          ) : (
+            <a onClick={() => editPage(record.id, { status: "draft" })}>禁用</a>
+          )}
+          <Divider type="vertical" />
+          <Link href={`/admin/page/editor/` + record.id}>
+            <a>编辑</a>
+          </Link>
+          <Divider type="vertical" />
+          <Popconfirm
+            title="确认删除这个页面？"
+            onConfirm={() => deleteArticle(record.id)}
+            okText="确认"
+            cancelText="取消"
+          >
+            <a>删除</a>
+          </Popconfirm>
+        </span>
+      );
+    }
+  };
+
+  return (
+    <AdminLayout>
+      <div className={style.wrapper}>
+        <Button
+          className={style.createBtn}
+          type="primary"
+          icon="plus"
+          onClick={() => router.push(`/admin/page/editor`)}
+        >
+          新建页面
+        </Button>
+        <Table
+          columns={[...columns, actionColumn]}
+          dataSource={pages}
+          rowKey={"id"}
+        />
+      </div>
+    </AdminLayout>
+  );
+};
+
+Page.getInitialProps = async () => {
+  const pages = await PageProvider.getPages();
+  return { pages };
+};
+
+export default Page;
