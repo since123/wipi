7b6939db535c309ab2eaf4add2badd08db848a8b
zhxuc <zhxuc7@163.com>
2020-02-10 17:25:50 +0800

    bugfix(server): fix get ip

--- server/src/modules/view/view.controller.ts
+++ server/src/modules/view/view.controller.ts
@@ -14,7 +14,6 @@ import { View } from './view.entity';
 
 function getClientIP(req) {
   const ip =
-    req.ip ||
     req.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
     (req.connection && req.connection.remoteAddress) || // 判断 connection 的远程 IP
     (req.socket && req.socket.remoteAddress) || // 判断后端的 socket 的 IP
